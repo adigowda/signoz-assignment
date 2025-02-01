c66bb22cc503d3a28aefb2fb0ca9c29be31f9f3a
adigowda <adithya.hr77@gmail.com>
2025-02-01 20:46:02 +0530

    feat: add jotai store for state management

---
+++ src/store/store.tsx
@@ -0,0 +1,10 @@
+import { atom } from "jotai";
+import { atomWithStorage } from "jotai/utils";
+import { EventInput as IEvent } from "@fullcalendar/core";
+import { withImmer } from "jotai-immer";
+
+export const eventsAtom = withImmer(
+  atomWithStorage<{ [key: string]: IEvent }>("events-data", {})
+);
+
+export const activeEventAtomId = atom(null);
