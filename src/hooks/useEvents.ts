e496c574bc1979d10e7d9788b2557ecff5111e71
adigowda <adithya.hr77@gmail.com>
2025-02-01 20:51:58 +0530

    feat: add useEvents hook

---
+++ src/hooks/useEvents.ts
@@ -0,0 +1,37 @@
+import { useAtom } from "jotai";
+import { eventsAtom } from "../store/store";
+import { v4 as uuidv4 } from "uuid";
+import { EventInput as IEvent } from "@fullcalendar/core";
+import { useMemo } from "react";
+
+export const useEvents = () => {
+  const [calendarEvents, setCalendarEvents] = useAtom(eventsAtom);
+  
+  const createEvent = (event: IEvent) => {
+    const id = uuidv4();
+    setCalendarEvents((calendarEvents) => {
+      calendarEvents[id] = event;
+    });
+  };
+
+  const updateEvent = (id: string, updatedEvent: IEvent) => {
+    setCalendarEvents((calendarEvents) => {
+      calendarEvents[id] = updatedEvent;
+    });
+  };
+
+  const events = useMemo(
+    () =>
+      Object.keys(calendarEvents).map((key) => ({
+        key,
+        ...calendarEvents[key],
+      })),
+    [calendarEvents]
+  );
+
+  return {
+    events,
+    updateEvent,
+    createEvent
+  };
+};
