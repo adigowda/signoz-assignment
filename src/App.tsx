5715429d5594cb30dd87002cce84985ff7f2a9cd
adigowda <adithya.hr77@gmail.com>
2025-02-03 04:01:11 +0530

    chore: remove App.css & rename index.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import "./App.css";
 import { Calendar } from "./components/Calendar/Calendar";
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 function App() {
   return (
-    <>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <Calendar />
-      </LocalizationProvider>
-    </>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Calendar />
+    </LocalizationProvider>
   );
 }
 
