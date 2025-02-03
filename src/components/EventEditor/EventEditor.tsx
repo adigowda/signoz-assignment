2cefd6cfd61332f1478444d4e6a7708c1d54c7b1
adigowda <adithya.hr77@gmail.com>
2025-02-03 15:43:19 +0530

    feat: update EventEditor to be responsive

--- src/components/EventEditor/EventEditor.tsx
+++ src/components/EventEditor/EventEditor.tsx
@@ -64,88 +64,90 @@ export function EventEditor(props: IEventEditorProps): JSX.Element {
   };
 
   return (
-    <div
-      className="bg-[#F0F4F9] border-[#039ae5] border-l-[20px] shadow-2xl flex flex-col gap-6 z-10 p-6 w-[500px] absolute bottom-1/2 translate-y-1/2 left-1/2 -translate-x-1/2 rounded-2xl event-editor"
-      style={{
-        borderColor: event.color,
-      }}
-    >
-      <div className="flex items-center gap-4 justify-end">
-        {event.id && (
-          <>
-            <img
-              src={Edit}
-              className="w-5 cursor-pointer"
-              onClick={() => {
-                setIsEditing(true);
-                titleInputRef.current?.focus();
-              }}
-            />
-            <img
-              src={Delete}
-              className="w-5 cursor-pointer"
-              onClick={() => onDelete(event)}
-            />
-          </>
-        )}
-        <img
-          src={Cross}
-          className="w-5 cursor-pointer ml-4"
-          onClick={onClose}
-        />
-      </div>
-      <input
-        ref={titleInputRef}
-        placeholder="Add title *"
-        value={event.title}
-        autoFocus={!event.id}
-        onChange={(e) => handleChange({ title: e.target.value })}
-        className="text-[20px] left-1/2 w-full px-2 pb-1 border-b-[#C4C7C5] outline-none border-b-[1px] focus:border-b-[#0b57d0] focus:border-b-[2px] hover:border-b-[#000000de]"
-      />
-      <div className="flex gap-4 items-center">
-        <DatePicker value={startDate} onChange={onStartDateChange} />
-        {isMultiDayEvent && (
-          <>
-            <span> - </span>
-            <DatePicker
-              value={endDate}
-              onChange={(value) => handleChange({ end: value?.toDate() })}
-              shouldDisableDate={(selectedDate) =>
-                selectedDate.toDate() < startDate.toDate()
-              }
-            />
-          </>
-        )}
-      </div>
-      <div className="flex gap-2 items-center">
-        <TimePicker
-          value={startDate}
-          onChange={(value) => handleChange({ start: value?.toDate() })}
-        />
-        <p>to</p>
-        <TimePicker
-          value={endDate}
-          onChange={(value) => handleChange({ end: value?.toDate() })}
-          shouldDisableTime={(selectedDate) =>
-            selectedDate.valueOf() < startDate.valueOf()
-          }
-        />
-      </div>
-      <ColorPicker
-        selectedColor={event.color}
-        onColorChange={(color) => handleChange({ color })}
-      />
-      <button
-        onClick={() => onSave(event)}
-        className={classNames(
-          "relative bg-[#0b57d0] py-2 px-4 cursor-pointer text-white rounded-full w-fit left-full -translate-x-full invisible",
-          {
-            visible: event.title && isEditing && !isError,
-          }
-        )}
+    <div className="z-10 absolute top-0 left-0 flex w-full h-screen items-center justify-center">
+      <div
+        className="bg-[#F0F4F9] border-[#039ae5] border-l-[20px] shadow-2xl flex flex-col gap-6 z-10 p-6 max-w-[420px] rounded-2xl event-editor self-center mx-auto"
+        style={{
+          borderColor: event.color,
+        }}
       >
-        Save
-      </button>
+        <div className="flex items-center gap-4 justify-end">
+          {event.id && (
+            <>
+              <img
+                src={Edit}
+                className="w-5 cursor-pointer"
+                onClick={() => {
+                  setIsEditing(true);
+                  titleInputRef.current?.focus();
+                }}
+              />
+              <img
+                src={Delete}
+                className="w-5 cursor-pointer"
+                onClick={() => onDelete(event)}
+              />
+            </>
+          )}
+          <img
+            src={Cross}
+            className="w-5 cursor-pointer ml-4"
+            onClick={onClose}
+          />
+        </div>
+        <input
+          ref={titleInputRef}
+          placeholder="Add title *"
+          value={event.title}
+          autoFocus={!event.id}
+          onChange={(e) => handleChange({ title: e.target.value })}
+          className="text-[20px] left-1/2 w-full px-2 pb-1 border-b-[#C4C7C5] outline-none border-b-[1px] focus:border-b-[#0b57d0] focus:border-b-[2px] hover:border-b-[#000000de]"
+        />
+        <div className="flex gap-4 items-center">
+          <DatePicker value={startDate} onChange={onStartDateChange} />
+          {isMultiDayEvent && (
+            <>
+              <span> - </span>
+              <DatePicker
+                value={endDate}
+                onChange={(value) => handleChange({ end: value?.toDate() })}
+                shouldDisableDate={(selectedDate) =>
+                  selectedDate.toDate() < startDate.toDate()
+                }
+              />
+            </>
+          )}
+        </div>
+        <div className="flex gap-2 items-center">
+          <TimePicker
+            value={startDate}
+            onChange={(value) => handleChange({ start: value?.toDate() })}
+          />
+          <p>to</p>
+          <TimePicker
+            value={endDate}
+            onChange={(value) => handleChange({ end: value?.toDate() })}
+            shouldDisableTime={(selectedDate) =>
+              selectedDate.valueOf() < startDate.valueOf()
+            }
+          />
+        </div>
+        <ColorPicker
+          selectedColor={event.color}
+          onColorChange={(color) => handleChange({ color })}
+        />
+        <button
+          onClick={() => onSave(event)}
+          className={classNames(
+            "relative bg-[#0b57d0] py-2 px-4 cursor-pointer text-white rounded-full w-fit left-full -translate-x-full invisible",
+            {
+              visible: event.title && isEditing && !isError,
+            }
+          )}
+        >
+          Save
+        </button>
+      </div>
     </div>
   );
 }
